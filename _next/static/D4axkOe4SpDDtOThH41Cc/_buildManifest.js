e847fe9f3ffa9bb3c9cce98b875e087150826582
sh1ma <sh1ma@users.noreply.github.com>
2022-05-17 14:46:34 +0000

    deploy: 74bc05bdd2171d20225c7378aa0d13462d151c3e

--- _next/static/D4axkOe4SpDDtOThH41Cc/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/cb1608f2-f7abf0afa0dfdaf2.js","static/chunks/814-ef8af0832aa83545.js","static/chunks/pages/index-0a2b053019c45113.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
